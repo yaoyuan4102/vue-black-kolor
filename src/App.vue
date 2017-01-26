964f540b73737ce005826c95e3c693298eb4382f
yaoyuan4102 <yaoyuan4102@gmail.com>
2017-01-26 12:34:54 -0600

    header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
+  <div id="">
+    <common-header></common-header>
     <img src="./assets/logo.png">
     <hello></hello>
+    <common-footer></common-footer>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+  import Hello from './components/Hello'
+  import CommonHeader from './components/common/Header.vue';
+  import CommonFooter from './components/common/Footer.vue';
 
-export default {
-  name: 'app',
-  components: {
-    Hello
+  export default {
+    name: 'app',
+    components: {
+      Hello,
+      CommonHeader,
+      CommonFooter
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/common/Header.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="hello">
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brand</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Paints</a></li>
+                        <li><a href="#">Events</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Support</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
